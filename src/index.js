8a194ffec95027befbb088e4583182ef21a78f94
psharif <psharif2788@gmail.com>
2018-07-12 19:25:10 -0700

    Added Keys in a different location

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-/// Youtube API Key - AIzaSyAU3i4DRpEuIq7CI50bgXN0BYnVYm59Nik
-const API_KEY = 'AIzaSyAU3i4DRpEuIq7CI50bgXN0BYnVYm59Nik';
+import API_KEY from './keys';
 
 // Create a new component. This component should produce some HTML.
 
---
+++ src/keys.js
@@ -0,0 +1,3 @@
+const API_KEY = 'AIzaSyAU3i4DRpEuIq7CI50bgXN0BYnVYm59Nik';
+
+export default API_KEY;
